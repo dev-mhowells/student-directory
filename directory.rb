54aba75a2c2c37e9a1ab64e3cae591f4d4e702a7
mike <dev.mhowells@gmail.com>
2022-10-31 14:10:14 +0000

    refactored to create interactive menu in loop

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 #first we print the list of students
-students = [
-{name: "Dr. Hanibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Kreger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Jeoffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
+# students = [
+# {name: "Dr. Hanibal Lecter", cohort: :november},
+# {name: "Darth Vader", cohort: :november},
+# {name: "Nurse Ratched", cohort: :november},
+# {name: "Michael Corleone", cohort: :november},
+# {name: "Alex DeLarge", cohort: :november},
+# {name: "The Wicked Witch of the West", cohort: :november},
+# {name: "Terminator", cohort: :november},
+# {name: "Freddy Kreger", cohort: :november},
+# {name: "The Joker", cohort: :november},
+# {name: "Jeoffrey Baratheon", cohort: :november},
+# {name: "Norman Bates", cohort: :november}
+# ]
 
 def input_students
     puts "Please enter the names of the students"
@@ -45,8 +45,34 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
+def interactive_menu
+    students = []
+    loop do
+        puts "1. Input the students"
+        puts "2. Show the students"
+        puts "9. Exit"
+        
+        selection = gets.chomp
+        
+        case selection
+            when "1"
+                students = input_students
+            when "2"
+                print_header
+                print(students)
+                print_footer(students)
+            when "9"
+                exit
+            else
+                puts "I don't know what you meant, try again"
+        end
+        
+    end
+end
 
-print_header
-print(students)
-print_footer(students)
+# students = input_students
+
+# print_header
+# print(students)
+# print_footer(students)
+interactive_menu
